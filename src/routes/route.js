0d8c868c16bb51dbde119b7619f05dbce66f0cf4
shi123shir <shishirsharma453@gmail.com>
2022-08-10 15:47:38 +0530

    cool

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,52 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+let players =
+    [
+        {
+            "name": "manish",
+            "dob": "1/1/1995",
+            "gender": "male",
+            "city": "jalandhar",
+            "sports": [
+                "swimming"
+            ]
+        },
+        {
+            "name": "gopal",
+            "dob": "1/09/1995",
+            "gender": "male",
+            "city": "delhi",
+            "sports": [
+                "soccer"
+            ],
+        },
+        {
+            "name": "lokesh",
+            "dob": "1/1/1990",
+            "gender": "male",
+            "city": "mumbai",
+            "sports": [
+                "soccer"
+            ],
+        },
+    ]
+
+router.post('/players', function (req, res) {
+    let ele = req.body
+
+    for (i = 0; i < players.length; i++) {
+        if (players[i].name !== ele.name) {
+            
+            return res.send("player already exist"  )
+        }
+    }
+    players.push(ele)
+     res.send({ data: players , status: true }  )
+        
+        
+})
+
 module.exports = router;
+
+
