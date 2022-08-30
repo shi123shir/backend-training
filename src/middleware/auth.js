14d796fd571af7b96ffc7ea3b4d202cc71f95eb5
shi123shir <110589155+shi123shir@users.noreply.github.com>
2022-08-30 17:06:47 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken")
 
 const authenticate = function(req, res, next) {
    let token = req.headers["x-auth-token"]
-   if(!token)token = req.headers["x-auth-token"]
    if (!token)return res.send({status: false, msg: "token must be present"})
  let decodedToken = jwt.verify(token, "functionup-thorium")
   if(!decodedToken){
